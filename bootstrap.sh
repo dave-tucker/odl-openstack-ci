fd1bea97eb3cfb975b1c3067d3cdc590ac693d6c
Kyle Mestery <kmestery@cisco.com>
2014-07-10 11:06:07 -0500

    Add deltarpm to list of installed packages.
    This saves time by using deltas instead of full RPMs when installing and
    updating packages here.
    
    Signed-off-by: Kyle Mestery <mestery@noironetworks.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install -q -y git python python-crypto python-devel python-lxml python-setuptools @development-tools libxml2-devel libxslt-devel libffi-devel
+yum install -q -y deltarpm git python python-crypto python-devel python-lxml python-setuptools @development-tools libxml2-devel libxslt-devel libffi-devel
 
 if [ ! -f /etc/udev/rules.d/80-net-setup-link.rules ]; then
     ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
